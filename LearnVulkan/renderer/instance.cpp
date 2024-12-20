cacef2f577eb2513be7512a89394caa3391ef88e
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-19 21:39:27 -0500

    Added MacOS compatibility flags

--- LearnVulkan/renderer/instance.cpp
+++ LearnVulkan/renderer/instance.cpp
@@ -9,19 +9,25 @@
 #include <iostream>
 #include <string>
 
+#ifdef LV_HOST_OS
+#if LV_HOST_OS == Darwin
+#define MacOS
+#endif
+#endif
+
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
-#else 
+#else
 const bool enableValidationLayers = true;
 #endif // !NDEBUG
 
 // TODO: Add VkDebugUtilsMessengerEXT
 
-namespace lv {
+namespace lv
+{
 
-	static const std::vector<const char*> validationLayers = {
-		"VK_LAYER_KHRONOS_validation"
-	};
+	static const std::vector<const char *> validationLayers = {
+		"VK_LAYER_KHRONOS_validation"};
 	static const uint32_t validationLayerCount = (uint32_t)validationLayers.size();
 
 	lv::VulkanInstance::VulkanInstance(std::string applicationName, std::string engineName)
@@ -29,14 +35,19 @@ namespace lv {
 		m_Version = vk::enumerateInstanceVersion();
 
 		std::vector<vk::ExtensionProperties> availableExtensions = CheckExtensionSupport();
-		std::vector<const char*> extensions;
+		std::vector<const char *> extensions;
+#ifdef MacOS
+		extensions.emplace_back(vk::KHRPortabilityEnumerationExtensionName);
+#endif
 		uint32_t extensionCount = (uint32_t)extensions.size();
 
-		for (const auto& extension : availableExtensions) {
+		for (const auto &extension : availableExtensions)
+		{
 			extensions.emplace_back(extension.extensionName);
 		}
 
-		if (enableValidationLayers && !CheckValidationLayerSupport()) {
+		if (enableValidationLayers && !CheckValidationLayerSupport())
+		{
 			throw std::runtime_error("validation layers requested, but not available!");
 		}
 
@@ -45,41 +56,43 @@ namespace lv {
 			VK_MAKE_VERSION(
 				applicationVersion.Major,
 				applicationVersion.Minor,
-				applicationVersion.Patch
-			),
+				applicationVersion.Patch),
 			engineName.c_str(),
 			VK_MAKE_VERSION(
 				applicationVersion.Major,
 				applicationVersion.Minor,
-				applicationVersion.Patch
-			),
-			m_Version
-		);
+				applicationVersion.Patch),
+			m_Version);
 
 		vk::InstanceCreateInfo createInfo{};
-		if (enableValidationLayers) {
+		if (enableValidationLayers)
+		{
 			createInfo = vk::InstanceCreateInfo(
 				vk::InstanceCreateFlags(),
 				&appInfo,
 				validationLayerCount,
 				validationLayers.data(),
 				extensionCount,
-				extensions.data()
-			);
+				extensions.data());
 		}
-		else {
+		else
+		{
 			createInfo = vk::InstanceCreateInfo(
 				vk::InstanceCreateFlags(),
 				&appInfo,
 				0,
 				nullptr,
 				extensionCount,
-				extensions.data()
-			);
+				extensions.data());
 		}
 
+#ifdef MacOS
+		createInfo.flags = vk::InstanceCreateFlags(VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR);
+#endif
+
 		m_Instance = vk::createInstance(createInfo);
-		if (!m_Instance) {
+		if (!m_Instance)
+		{
 			throw std::runtime_error("unable to create vulkan instance!");
 		}
 	}
@@ -93,34 +106,39 @@ namespace lv {
 	{
 		uint32_t glfwExtensionCount = 0;
 
-		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-		if (!glfwExtensions) {
+		const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		if (!glfwExtensions)
+		{
 			throw std::runtime_error("cannot get glfw extensions!");
 		}
 
-		std::vector<const char*> glfwExtensionsStrings;
+		std::vector<const char *> glfwExtensionsStrings;
 
 		std::cout << "Required extensions: \n";
-		for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+		for (uint32_t i = 0; i < glfwExtensionCount; i++)
+		{
 			std::cout << "\t" << glfwExtensions[i] << "\n";
 			glfwExtensionsStrings.emplace_back(glfwExtensions[i]);
 		}
 
 		std::vector<vk::ExtensionProperties> availableExtensions = vk::enumerateInstanceExtensionProperties();
-		std::vector<const char*> enabledExtensions;
+		std::vector<const char *> enabledExtensions;
 
 		std::cout << "Available Extensions: \n";
-		for (const auto& extension : availableExtensions) {
+		for (const auto &extension : availableExtensions)
+		{
 			enabledExtensions.emplace_back(extension.extensionName);
 			std::cout << "\t" << extension.extensionName.data() << "\n";
 		}
 
 		bool found = false;
-		for (uint32_t i = 0; i < glfwExtensionCount; i++) {
-			found = std::find(enabledExtensions.begin(), enabledExtensions.end()-1, glfwExtensionsStrings[i]) != enabledExtensions.end();
+		for (uint32_t i = 0; i < glfwExtensionCount; i++)
+		{
+			found = std::find(enabledExtensions.begin(), enabledExtensions.end() - 1, glfwExtensionsStrings[i]) != enabledExtensions.end();
 		};
 
-		if (!found) {
+		if (!found)
+		{
 			throw std::runtime_error("Required extensions are not supported!");
 		}
 
@@ -132,14 +150,18 @@ namespace lv {
 		std::vector<vk::LayerProperties> layers = vk::enumerateInstanceLayerProperties();
 
 		std::cout << "Available Layers: \n";
-		for (uint32_t i = 0; i < layers.size(); i++) {
+		for (uint32_t i = 0; i < layers.size(); i++)
+		{
 			std::cout << "\t" << layers[i].layerName << std::endl;
 		}
 
 		bool layerFound = false;
-		for (const char* layerName : validationLayers) {
-			for (const auto& layerProperties : layers) {
-				if (strcmp(layerName, layerProperties.layerName) == 0) {
+		for (const char *layerName : validationLayers)
+		{
+			for (const auto &layerProperties : layers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
 					layerFound = true;
 					std::cout << "Found Requested Layer: " << layerProperties.layerName << std::endl;
 					break;
